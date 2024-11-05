8133640e0946700f5122085d5773f5d4288d6e85
Yuxin Ma <mayuxin98@gmail.com>
2024-11-05 18:41:06 -0500

    run size_generalization for 10 trials.

--- DeepSet/size_generalization.py
+++ DeepSet/size_generalization.py
@@ -36,6 +36,7 @@ def eval(model, params):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--training_size", type=int, default=500)
+    parser.add_argument("--num_trials", type=int, default=10)
     parser.add_argument("--num_layers", type=int, default=3)
     parser.add_argument("--hidden_channels", type=int, default=50)
     parser.add_argument("--set_channels", type=int, default=50)
@@ -78,21 +79,36 @@ if __name__ == '__main__':
     
     for task_id in [1,2,3,4]:
         params["task_id"] = task_id
+        
+        mse_normalized_list = []
+        mse_unnormalized_list = []
+        for trial in range(args.num_trials):
+            params["model"]["normalized"] = True
+            params["training_seed"] = trial + 1
+            model_normalized = train(params)
+            mse_normalized = eval(model_normalized, params)
+            mse_normalized_list.append(mse_normalized[-1])
 
-        params["model"]["normalized"] = True
-        model_normalized = train(params)
-        mse_normalized = eval(model_normalized, params)
+            params["model"]["normalized"] = False
+            model_unnormalized = train(params)
+            mse_unnormalized = eval(model_unnormalized, params)
+            mse_unnormalized_list.append(mse_unnormalized[-1])
 
-        params["model"]["normalized"] = False
-        model_unnormalized = train(params)
-        mse_unnormalized = eval(model_unnormalized, params)
-    
-        plt.plot(np.arange(1000,5000,500), mse_normalized, label='Normalized')
-        plt.plot(np.arange(1000,5000,500), mse_unnormalized, label='Unnormalized')
+        mean_mse_normalized = np.mean(mse_normalized_list, axis=0)
+        mean_mse_unnormalized = np.mean(mse_unnormalized_list, axis=0)
+        min_mse_normalized = np.min(mse_normalized_list, axis=0)
+        max_mse_normalized = np.max(mse_normalized_list, axis=0)
+        min_mse_unnormalized = np.min(mse_unnormalized_list, axis=0)
+        max_mse_unnormalized = np.max(mse_unnormalized_list, axis=0)
+        
+        plt.plot(np.arange(1000,5000,500), mean_mse_normalized, label='Normalized')
+        plt.fill_between(np.arange(1000,5000,500), min_mse_normalized, max_mse_normalized, alpha=0.3)
+        plt.plot(np.arange(1000,5000,500), mean_mse_unnormalized, label='Unnormalized')
+        plt.fill_between(np.arange(1000,5000,500), min_mse_unnormalized, max_mse_unnormalized, alpha=0.3)
         plt.xlabel('Test set size (N)')
         plt.ylabel('Test MSE')
         plt.title(f'Task {params["task_id"]}')
         plt.legend()
         plt.xscale('log')
         plt.yscale('log')
-        plt.savefig(os.path.join(params["log_dir"], f'size_generalization/task{params["task_id"]}_plot.png'))   
+        plt.savefig(os.path.join(params["log_dir"], f'size_generalization/task{params["task_id"]}_plot.png'))
